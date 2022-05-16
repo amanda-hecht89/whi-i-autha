b55c63b5e6f0307aaac351ebea6ece669af51905
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-16 12:14:23 -0600

    sign up button

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { signupUser } from "./fetch-utils";
+
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
@@ -7,5 +9,11 @@ const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const user = await signupUser(data.get('email'), data.get('password'));
+    }
+})
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
